4e760737324e9dcec59df9509e8442aecab149d7
juanyjuan4 <jdwwhetten2@gmail.com>
2023-03-31 22:31:16 -0600

    Removed commented out repeat code in univariate/mod.rs

--- src/univariate/mod.rs
+++ src/univariate/mod.rs
@@ -2,11 +2,6 @@ pub mod doubling;
 pub mod shrinkage;
 pub mod stepping_out;
 
-// pub trait UnivariateTarget {
-//     fn evaluate(&mut self, x: f64) -> f64;
-//     fn on_log_scale(&self) -> bool;
-// }
-
 pub trait UnivariateTarget {
     fn evaluate(&mut self, x: f64) -> f64;
     fn on_log_scale(&self) -> bool;
